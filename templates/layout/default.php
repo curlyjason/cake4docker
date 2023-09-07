654059111ec767301510395cb861c94cf5610d4a
dreamingmind <ddrake@dreamingmind.com>
2023-09-07 14:08:32 -0700

    fix page titles

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,7 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$cakeDescription = 'CakePHP4x/PHP8.2 Dock';
 ?>
 <!DOCTYPE html>
 <html>
